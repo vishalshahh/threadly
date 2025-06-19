fd034e29caf74707db1e2595c71ed368243901e2
Vishal Shah <vishal12kl@gmail.com>
2025-06-19 22:45:41 +0530

    (feat): implemented auth authentication using github

---
+++ auth.ts
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import GithubProvider from "next-auth/providers/github";
+import { prisma } from "@/lib";
+
+if (!process.env.GITHUB_CLIENT_ID || !process.env.GITHUB_CLIENT_SECRET) {
+    throw new Error("GITHUB_ID and GITHUB_SECRET must be set")
+}
+
+export const {handlers:{GET, POST}, auth,signIn, signOut} = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID!,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET!
+        })
+    ],
+    callbacks: {
+        async session({ user, session }) {
+            if (session && user) {
+                session.user.id = userid
+            }
+            return session
+        }
+    }
+})
